2d2216662acccb69f60c8857ead03561154dc031
Abdulaleam <167490133+Abdulaleam@users.noreply.github.com>
2026-08-03 04:53:51 +0300

    added the emain file i will be callingg and linking all events i will be making in , i will also start working on the tick counter or the manager handler for the mod now

---
+++ src/main/java/rainy/choas/ChoasEventRegistry.java
@@ -0,0 +1,39 @@
+package rainy.choas;
+
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+
+import java.util.List;
+import java.util.Random;
+
+public class ChoasEventRegistry {
+
+    public interface ChoasEvent {
+        String execute(ServerPlayerEntity player);
+
+    }
+
+    private static final Random RANDOM = new Random();
+
+
+
+    private static final List<ChoasEvent> EVENTS = List.of(
+
+            // add the events i will make here later on
+
+    );
+
+    public static void BoomRandomEvents(ServerPlayerEntity player) {
+        ChoasEvent event = EVENTS.get(RANDOM.nextInt(EVENTS.size()));
+        String message = event.execute(player);
+
+        MinecraftServer server = player.getServer();
+        if (server != null) {
+            server.getPlayerManager().broadcast(Text.literal("§6[Chaos] §e" + message), false);
+        }
+
+        player.sendMessage(Text.literal("§d" + message), true);
+
+    }
+}
